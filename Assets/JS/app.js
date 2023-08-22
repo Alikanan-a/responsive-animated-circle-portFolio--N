2913b423f314597b0a0f0fcd695b314f49855bf7
alikanan <a.kanan2222@gmail.com>
2023-08-22 11:46:00 +0430

    add scrollreveall

--- Assets/JS/app.js
+++ Assets/JS/app.js
@@ -91,7 +91,16 @@ var swiper = new Swiper(".mySwiper", {
 
 
 // /*=============== sticky navbar ===============*/
-ScrollReveal({ reset: true });
+ScrollReveal({
+  // reset: true,
+  distance: '80px',
+  duration: 2000,
+  delay: 200
+});
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img img, .services-container, .portfolio-box, .testimonial-wrapper, .contact form  ', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img img', { origin: 'left' });
+ScrollReveal().reveal('.home-content h3, .home-content p, .about-content,  ', { origin: 'right' });
 // console.log('hello')
 // <!--=============== colors ===============-->
 const blue = document.querySelector('#blue')
